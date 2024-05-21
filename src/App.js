970c95ad4fb0c177c6160b160d3d410c7fa535e9
gomdori2 <dbstjdghks21@naver.com>
2024-05-21 18:10:20 +0900

    docs: axios 정리(1)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  deleteTodos,
+  getOneTodos,
+  getTodos,
+  postTodos,
+  putTodos,
+} from "./apis/todos";
 
 function App() {
   return (
     <div className="App">
+      <div>
+        <button
+          onClick={() => {
+            getOneTodos(2);
+          }}
+        >
+          읽기
+        </button>
+        <button onClick={() => {}}>하나만 읽기</button>
+        <button onClick={() => {}}>쓰기</button>
+        <button onClick={() => {}}>삭제</button>
+        <button onClick={() => {}}>수정</button>
+      </div>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
